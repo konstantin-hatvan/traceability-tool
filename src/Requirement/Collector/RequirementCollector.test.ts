f5c740c5c011098959e805e75646d27c019693d3
Konstantin Hatvan <k.hatvan@gmx.at>
2020-07-18 08:25:00 +0200

    Add mock-fs

--- src/Requirement/Collector/RequirementCollector.test.ts
+++ src/Requirement/Collector/RequirementCollector.test.ts
@@ -1,24 +1,68 @@
-import * as path from 'path';
-import * as fs from 'fs';
+import path from 'path';
+import fs from 'fs';
+import mock from 'mock-fs';
 import RequirementCollector from './RequirementCollector';
-import { rootDirectory } from '../../../test/Project/configuration';
-import { setup } from '../../../test/Project/setup';
-import { teardown } from '../../../test/Project/teardown';
-
-// Prepare tests
-beforeAll(() => {
-    setup(rootDirectory);
-});
-
-afterAll(() => {
-    teardown(rootDirectory);
-});
 
 // Execute tests
 describe('RequirementCollector', () => {
+    beforeEach(() => {
+        mock({
+            'docs': {
+                'requirement_01.md': `---
+id: REQ_01
+test: test
+---
+
+# Requirement 1 Test
+
+- This is a list
+- Another List item
+`,
+                'requirement_02.md': `---
+id: REQ_02
+---
+
+# Requirement 2 Test
+
+This time a paragraph and a table under this block
+
+| Equivalenzklassen              | Anzahl der Testfälle |
+|--------------------------------|----------------------|
+| Input falsch (falsches Schema) | 1                    |
+| Input falsch (nonnumeric)      | 1                    |
+| Input falsch (negativ)         | 1                    |
+| Stunde > 25                    | 1                    |
+| Minute > 99                    | 0                    |
+`
+            },
+            'src': {
+                'Resources': {
+                    'Private': {
+                        'main.ts': `console.log("Hello World!");
+`,
+                        'styles.scss': `html {
+    background: blue;
+}
+`,
+                    },
+                    'Public': {
+                        'main.js': `console.log("Hello World!");
+`,
+                        'styles.css': `html {
+    background: blue;
+}
+`,
+                    }
+                }
+            }
+        });
+    });
+
+    afterEach(mock.restore);
+
     describe('.collect()', () => {
         test('returns an array of strings', () => {
-            const requirements = RequirementCollector.collect(rootDirectory);
+            const requirements = RequirementCollector.collect('docs');
 
             requirements.forEach(requirement => {
                 expect(requirement).toMatch(/[a-z0-9]*/);
@@ -26,7 +70,7 @@ describe('RequirementCollector', () => {
         });
 
         test('returns an array of filesystem paths', () => {
-            const requirements = RequirementCollector.collect(rootDirectory);
+            const requirements = RequirementCollector.collect('docs');
 
             requirements.forEach(requirement => {
                 expect(fs.statSync(requirement).isFile()).toBeTruthy();
@@ -34,7 +78,7 @@ describe('RequirementCollector', () => {
         });
 
         test('returns an array of markdown files', () => {
-            const requirements = RequirementCollector.collect(rootDirectory);
+            const requirements = RequirementCollector.collect('docs');
 
             requirements.forEach(requirement => {
                 expect(path.parse(requirement).ext).toEqual('.md');
--- test/Project/teardown.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from 'fs';
-
-export const teardown = (rootDirectory: string) => {
-    if (fs.existsSync(rootDirectory)) {
-        fs.rmdirSync(rootDirectory, {
-            recursive: true,
-        });
-    }
-};
