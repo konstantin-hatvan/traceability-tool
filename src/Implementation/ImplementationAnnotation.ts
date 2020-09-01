7968776083a12fd7ecc7fd08e43eaf52fa40a4ba
Konstantin Hatvan <k.hatvan@limesoda.com>
2020-09-01 13:42:01 +0200

    Add code comments

--- src/Implementation/ImplementationAnnotation.ts
+++ src/Implementation/ImplementationAnnotation.ts
@@ -4,18 +4,41 @@
 
 import { constants } from '../Shared/constants';
 
+/**
+ * Remove any characters before the requirement annotation from the given string
+ * @param str A string
+ */
 const stripComment = (str: string): string => str.substring(str.indexOf(constants.requirement.annotation)).trim();
 
+/**
+ * Remove the requirement annotation from the given string
+ * @param str A string
+ */
 const stripAnnotation = (str: string): string => str.substring(constants.requirement.annotation.length).trim();
 
+/**
+ * Remove any whitespace from the given string
+ * @param str A string
+ */
 const stripWhitespace = (str: string): string => str.replace(/\s*/g, '');
 
+/**
+ * A collection of sanitizers to perform on a string
+ */
 const sanitizers = [
     stripComment,
     stripAnnotation,
     stripWhitespace,
 ];
 
+/**
+ * Sanitize the given string
+ * @param lineWithAnnotation A string containing a requirement annotation
+ */
 const sanitize = (lineWithAnnotation: string): string => sanitizers.reduce((result, sanitizer) => sanitizer(result), lineWithAnnotation);
 
+/**
+ * Parse the given requirement annotation into a list of requirement identifiers
+ * @param lineWithAnnotation A string containing a requirement annotation
+ */
 export const parse = (lineWithAnnotation: string): string[] => sanitize(lineWithAnnotation).split(',');
