2777cef29d74201da6bfb7e196b1700e2f008ba2
Konstantin Hatvan <k.hatvan@limesoda.com>
2020-09-06 12:20:50 +0200

    Add defaultConfiguration and configuration merge

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,25 @@
 import { cosmiconfigSync } from 'cosmiconfig';
 import { Configuration } from './Shared/types';
 import * as Application from './Application';
+import merge from 'lodash.merge';
 
 const explorer = cosmiconfigSync('tracey');
 const result = <Configuration>explorer.search()?.config;
 
-Application.main(result).then(() => {
+const defaultConfiguration: Configuration = {
+    implementation: {
+        startingpoint: '.',
+        annotation: '@requirement',
+        excludes: [],
+    },
+    requirement: {
+        excludes: [],
+        startingpoint: '.'
+    },
+};
+
+const configuration: Configuration = merge(defaultConfiguration, result);
+
+Application.main(configuration).then(() => {
     console.log('Process finished');
-})
+});
