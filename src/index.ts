45a80df181a63d6b166f614659d3d23dc4a81906
Konstantin Hatvan <k.hatvan@limesoda.com>
2020-09-08 17:05:12 +0200

    Fix alphabetical ordering

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,9 @@ const result = <Configuration>explorer.search()?.config;
 
 const defaultConfiguration: Configuration = {
     implementation: {
-        startingpoint: '.', /** @requirement #[ Imlementation/Collector ]# #( Implementation files will be collected from the file system starting at the configured startingpoint )# */
         annotation: '@requirement', /** @requirement #[ Implementation/Annotation ]# #( The annotation uses a default property )# */
         excludes: [], /** @requirement #[ Implementation/Collector ]# #( Implementation files can be excluded in the configuration )# */
+        startingpoint: '.', /** @requirement #[ Imlementation/Collector ]# #( Implementation files will be collected from the file system starting at the configured startingpoint )# */
     },
     requirement: {
         excludes: [], /** @requirement #[ Requirement/Collector ]# #( Requirements can be excluded in the configuration )# */
