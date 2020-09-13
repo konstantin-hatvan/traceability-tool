64bbf5784f9ca542f1ec72826e7d44be3605639c
Konstantin Hatvan <k.hatvan@limesoda.com>
2020-09-13 17:10:59 +0200

    Add factory tests

---
+++ src/Implementation/Factory/Factory.test.ts
@@ -0,0 +1,78 @@
+import { createImplementations } from './Factory';
+import mock from 'mock-fs';
+import { ImplementationConfiguration } from '../../Shared/types';
+
+describe('Implementation/Factory:createImplementations()', () => {
+    beforeEach(() => {
+        // console.log('beforeEach'); // workaround for mock-fs problem with console.log
+    });
+
+    afterEach(mock.restore);
+
+    test('creates correct data structure', async () => {
+        mock({
+            src: {
+                'main.ts': 'console.log("Hello world!"); /* @requirement #[ MyRequirement ]# #( My description )# */',
+            },
+        });
+
+        const files = [
+            'src/main.ts',
+        ];
+
+        const configuration: ImplementationConfiguration = {
+            startingpoint: 'src',
+            excludes: [],
+            annotation: '@requirement',
+        };
+
+        const result = await createImplementations(files, configuration);
+
+        expect(result).toEqual([
+            {
+                type: 'implementation',
+                file: 'src/main.ts',
+                line: 1,
+                requirement: 'MyRequirement',
+                description: 'My description'
+            },
+        ]);
+    });
+
+    test('creates multiple for a comma separated list of requirement identifiers', async () => {
+        mock({
+            src: {
+                'multiple.ts': 'console.log("Hello world!"); /* @requirement #[ MyRequirement, MySecondRequirement ]# #( My description )# */',
+            },
+        });
+
+        const files = [
+            'src/multiple.ts',
+        ];
+
+        const configuration: ImplementationConfiguration = {
+            startingpoint: 'src',
+            excludes: [],
+            annotation: '@requirement',
+        };
+
+        const result = await createImplementations(files, configuration);
+
+        expect(result).toEqual([
+            {
+                type: 'implementation',
+                file: 'src/multiple.ts',
+                line: 1,
+                requirement: 'MyRequirement',
+                description: 'My description'
+            },
+            {
+                type: 'implementation',
+                file: 'src/multiple.ts',
+                line: 1,
+                requirement: 'MySecondRequirement',
+                description: 'My description'
+            },
+        ]);
+    });
+});
---
+++ src/Requirement/Factory/Factory.test.ts
@@ -0,0 +1,49 @@
+import { createRequirements } from './Factory';
+import mock from 'mock-fs';
+
+describe('Requirement/Factory:createRequirements()', () => {
+    beforeEach(() => {
+        // console.log('beforeEach'); // workaround for mock-fs problem with console.log
+    });
+
+    afterEach(mock.restore);
+
+    test('creates correct data structure', async () => {
+        mock({
+            docs: {
+                'index.md': `---
+id: MyRequirement
+---
+
+# My Requirement
+`,
+            },
+        });
+
+        const files = [
+            'docs/index.md',
+        ];
+
+        const result = await createRequirements(files);
+
+        expect(result).toEqual([
+            {
+                type: 'requirement',
+                file: 'docs/index.md',
+                id: 'MyRequirement',
+                ast: expect.objectContaining({
+                    type: 'root',
+                    children: [
+                        expect.objectContaining({
+                            type: 'yaml',
+                            value: 'id: MyRequirement'
+                        }),
+                        expect.objectContaining({
+                            type: 'heading',
+                        }),
+                    ],
+                }),
+            },
+        ]);
+    });
+});
