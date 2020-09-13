7748ba2a8384e5b4d5f9728581a81e7d1fe8b5b0
Konstantin Hatvan <k.hatvan@limesoda.com>
2020-09-13 17:20:26 +0200

    Add tests for implementation list process

---
+++ src/Implementation/ListProcess/ListProcess.test.ts
@@ -0,0 +1,44 @@
+import mock from 'mock-fs';
+import { list } from './ListProcess';
+import { ImplementationConfiguration } from '../../Shared/types';
+
+beforeEach(() => {
+    // console.log('beforeEach'); // workaround for mock-fs problem with console.log
+});
+
+afterEach(mock.restore);
+
+describe('Implementation/ListProcess:list()', () => {
+    test('returns a collection of implementation data structures', async () => {
+        mock({
+            src: {
+                'main.ts': 'console.log("Hello world!"); /* @requirement #[ MyRequirement, MySecondRequirement ]# #( My description )# */',
+            },
+        });
+
+        const configuration: ImplementationConfiguration = {
+            startingpoint: 'src',
+            excludes: [],
+            annotation: '@requirement',
+        };
+
+        const result = await list(configuration);
+
+        expect(result).toEqual([
+            {
+                type: 'implementation',
+                file: 'src/main.ts',
+                requirement: 'MyRequirement',
+                line: 1,
+                description: 'My description'
+            },
+            {
+                type: 'implementation',
+                file: 'src/main.ts',
+                requirement: 'MySecondRequirement',
+                line: 1,
+                description: 'My description'
+            },
+        ]);
+    });
+});
