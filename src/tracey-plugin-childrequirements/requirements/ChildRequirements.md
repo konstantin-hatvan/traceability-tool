0284ba3660803fd36c36e769518e57cc92b74a2b
Konstantin Hatvan <k.hatvan@limesoda.com>
2020-10-19 21:49:50 +0200

    Add tracey-plugin-tracelinktable

---
+++ src/tracey-plugin-childrequirements/requirements/ChildRequirements.md
@@ -0,0 +1,11 @@
+---
+id: ChildRequirements
+---
+
+# Plugin: Child Requirements
+
+This plugin enables the developer to connect requirements in a parent-child relationship. These relationships will then be rendered as a table of tracelinks in each requirement.
+
+-   Developers can specify the parent requirement by using the frontmatter property `parent` on the child requirement
+-   The value of the `parent` property must be a valid requirement identifier
+-   Child requirements will be rendered as a table of tracelinks on the parent requirement
---
+++ src/tracey-plugin-tracelinktable/index.ts
@@ -0,0 +1,102 @@
+import { Requirement } from '../Requirement/types';
+import visit from 'unist-util-visit';
+import { TraceLink } from '../TraceLink/types';
+import { Plugin } from '../types';
+import { Table, HTML } from 'mdast';
+import { createHTML, createLink, createTable, createTableCell, createTableCellFromText, createTableRow, createText } from '../tracey-plugin-utility';
+import path from 'path';
+
+/**
+ * Create a markdown table wrapped in a HTML div from given tracelinks
+ * @param traceLinks A list of tracelinks
+ */
+const createTracelinktable = (traceLinks: TraceLink[]): (Table | HTML)[] => {
+    const tableRows = traceLinks.map(traceLink => {
+        const relativeLink = `${path.relative(path.parse(traceLink.destination.file).dir, traceLink.annotation.file)}#L${traceLink.annotation.line}`;
+
+        return createTableRow([
+            createTableCell(createLink(traceLink.annotation.file, relativeLink)),
+            createTableCell(createText(traceLink.annotation.line.toString())),
+            createTableCellFromText(traceLink.annotation.description),
+        ])
+    });
+
+    const tableHeaderRow = createTableRow([
+        createTableCell(createText('File')),
+        createTableCell(createText('Line')),
+        createTableCell(createText('Description')),
+    ]);
+
+    const table = createTable([
+        tableHeaderRow,
+        ...tableRows,
+    ]);;
+
+    const startBlock = createHTML('<div class="tracey tracey-plugin-tracelinktable">');
+    const endBlock = createHTML('</div>');
+
+    return [
+        startBlock,
+        table,
+        endBlock,
+    ];
+};
+
+/**
+ * Add tracelinks
+ * @param original A requirement
+ * @param traceLinks A list of tracelinks
+ */
+const updateTracelinks = (original: Requirement, traceLinks: TraceLink[]): Requirement => {
+    const requirement = { ...original };
+    const tracelinkTable = createTracelinktable(traceLinks);
+    let shouldAddChildRequirementsToBottom = true;
+
+    visit(requirement.ast, 'html', (node, index, parent) => {
+        if (node.value === '<div class="tracey tracey-plugin-tracelinktable">' && parent) {
+            parent.children.splice(index, tracelinkTable.length, ...tracelinkTable);
+            shouldAddChildRequirementsToBottom = false;
+        }
+    });
+
+    if (shouldAddChildRequirementsToBottom) {
+        requirement.ast.children.push(...tracelinkTable);
+    }
+
+    return requirement;
+};
+
+const removeTraceLinks = (original: Requirement): Requirement => {
+    const requirement = { ...original };
+
+    visit(requirement.ast, 'html', (node, index, parent) => {
+        if (node.value === '<div class="tracey tracey-plugin-tracelinktable">' && parent) {
+            parent.children.splice(index, 3);
+        }
+    });
+
+    return requirement;
+};
+
+/**
+ * Update tracelinks
+ * @param original A requirement
+ * @param traceLinks A list of trace links
+ */
+export const plugin: Plugin = ({ requirements, annotations, tracelinks }) => {
+    const updatedRequirements = requirements.map(requirement => {
+        const linkedTracelinks = tracelinks.filter(tracelink => tracelink.annotation.identifier === requirement.id);
+
+        if (linkedTracelinks.length) {
+            return updateTracelinks(requirement, linkedTracelinks);
+        }
+
+        return removeTraceLinks(requirement);
+    });
+
+    return {
+        requirements: updatedRequirements,
+        annotations,
+        tracelinks,
+    };
+};
